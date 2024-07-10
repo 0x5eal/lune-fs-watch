d46b708cc53f271e6225884733ebeab691f269b4
Erica Marigold <hi@devcomp.xyz>
2024-07-10 20:28:09 +0530

    feat: initial fs.watch implementation

--- crates/lune-std-fs/src/lib.rs
+++ crates/lune-std-fs/src/lib.rs
@@ -1,17 +1,21 @@
 #![allow(clippy::cargo_common_metadata)]
 
 use std::io::ErrorKind as IoErrorKind;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use bstr::{BString, ByteSlice};
 use mlua::prelude::*;
+use mlua_luau_scheduler::LuaSchedulerExt;
+use notify::{EventKind, RecursiveMode, Watcher};
 use tokio::fs;
 
 use lune_utils::TableBuilder;
+use watch::WatchOptions;
 
 mod copy;
 mod metadata;
 mod options;
+mod watch;
 
 use self::copy::copy;
 use self::metadata::FsMetadata;
@@ -37,6 +41,7 @@ pub fn module(lua: &Lua) -> LuaResult<LuaTable> {
         .with_async_function("isDir", fs_is_dir)?
         .with_async_function("move", fs_move)?
         .with_async_function("copy", fs_copy)?
+        .with_async_function("watch", fs_watch)?
         .build_readonly()
 }
 
@@ -123,4 +128,43 @@ async fn fs_move(_: &Lua, (from, to, options): (String, String, FsWriteOptions))
 
 async fn fs_copy(_: &Lua, (from, to, options): (String, String, FsWriteOptions)) -> LuaResult<()> {
     copy(from, to, options).await
+}
+
+async fn fs_watch(
+    lua: &Lua,
+    (root_path, options, handlers): (String, WatchOptions, LuaTable<'_>),
+) -> LuaResult<()> {
+    let (tx, mut rx) = tokio::sync::mpsc::channel(1);
+    let mut watcher = options.as_watcher(tx).into_lua_err()?;
+    let added_handler = handlers.get::<_, LuaFunction>("added").ok();
+    let removed_handler = handlers.get::<_, LuaFunction>("removed").ok();
+    let changed_handler = handlers.get::<_, LuaFunction>("changed").ok();
+
+    watcher
+        .watch(
+            &PathBuf::from(root_path),
+            if options.recursive {
+                RecursiveMode::Recursive
+            } else {
+                RecursiveMode::NonRecursive
+            },
+        )
+        .into_lua_err()?;
+
+    while let Some(res) = rx.recv().await {
+        let event = res.into_lua_err()?;
+        let handler = match event.kind {
+            EventKind::Any | EventKind::Other => continue,
+            EventKind::Access(_) => todo!(),
+            EventKind::Remove(_) => &removed_handler,
+            EventKind::Create(_) => &added_handler,
+            EventKind::Modify(_) => &changed_handler,
+        };
+
+        if let Some(handler) = handler {
+            lua.push_thread_back(handler, ())?;
+        }
+    }
+
+    Ok(())
 }
---
+++ crates/lune-std-fs/src/watch.rs
@@ -0,0 +1,48 @@
+use std::{default::Default, time::Duration};
+
+use mlua::prelude::*;
+use notify::{Config, Event, RecommendedWatcher, Watcher};
+
+#[derive(Default)]
+pub struct WatchOptions {
+    pub pattern: String,
+    pub recursive: bool,
+    pub watch_files: bool,
+    pub watch_diretories: bool,
+    pub interval: Option<u64>,
+}
+
+impl WatchOptions {
+    pub fn as_watcher(
+        &self,
+        tx: tokio::sync::mpsc::Sender<notify::Result<Event>>,
+    ) -> notify::Result<RecommendedWatcher> {
+        RecommendedWatcher::new(
+            move |res| tx.blocking_send(res).unwrap(),
+            Config::default().with_poll_interval(Duration::from_secs(self.interval.unwrap_or(30))),
+        )
+    }
+}
+
+impl FromLua<'_> for WatchOptions {
+    fn from_lua(value: LuaValue<'_>, _: &'_ mlua::Lua) -> LuaResult<Self> {
+        match value {
+            LuaValue::String(s) => Ok(Self {
+                pattern: s.to_str()?.to_string(),
+                ..Default::default()
+            }),
+            LuaValue::Table(t) => Ok(Self {
+                pattern: t.get("pattern")?,
+                recursive: t.get("recursive")?,
+                watch_files: t.get("watchFiles").unwrap_or_default(),
+                watch_diretories: t.get("watchDirectories").unwrap_or_default(),
+                interval: t.get("interval").unwrap_or_default(),
+            }),
+            other => Err(LuaError::FromLuaConversionError {
+                from: other.type_name(),
+                to: "WatchOptions",
+                message: Some("Argument must be of type string or table".to_string()),
+            }),
+        }
+    }
+}
